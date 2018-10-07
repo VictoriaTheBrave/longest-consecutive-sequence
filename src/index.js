4e8e7722abd0734d19402e68c5774bea45da0b8b
Victoria Likhuta <v1ktor1a09051994@gmail.com>
2018-10-07 20:04:51 +0300

    longest-consecutive-sequence/feature: implement function with required complexity

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 module.exports = function longestConsecutiveLength(array) {
-  // your solution here
+  if (array.length < 2) {
+    return array.length;
+  }
+  const set = new Set(array);
+  let longestConsecutive = 1;
+  for (let i = 0; i < array.length; i++) {
+    let left = array[i] - 1;
+    let right = array[i] + 1;
+    let count = 1;
+    while (set.has(left)) {
+      count++;
+      set.delete(left);
+      left--;
+    }
+    while (set.has(right)) {
+      count++;
+      set.delete(right);
+      right++;
+    }
+    longestConsecutive = Math.max(count, longestConsecutive);
+  }
+  return longestConsecutive;
 }
